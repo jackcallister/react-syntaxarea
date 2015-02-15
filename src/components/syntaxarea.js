08c245bf2454040065774b63bf043b06927545e2
jarsbe <jarsbe@gmail.com>
2015-02-15 20:01:31 +1300

    Remove blinking.

--- src/components/syntaxarea.js
+++ src/components/syntaxarea.js
@@ -5,45 +5,33 @@ var Parser = require('../helpers/parser');
 var Measurement = require('../helpers/measurement');
 var TextArea = require('react-textarea-autosize');
 
-var timers = [];
-
 var Syntaxarea = React.createClass({
 
   displayName: 'syntaxtarea',
 
-  _typingTimer: null,
-
   _onChange: function(e) {
-    this.setState({value: e.target.value});
+    this.setState({
+      value: e.target.value
+    });
     this._updateCaretPosition(e);
   },
 
   _updateCaretPosition: function(e) {
     var coordinates = Measurement(e.target, e.target.selectionEnd);
 
-    timers.forEach(function(timer) {
-      clearTimeout(timer);
-    });
-
     this.setState({
       caretTop: coordinates.top,
-      caretLeft: coordinates.left,
-      caretVisibilityClass: 'solid'
+      caretLeft: coordinates.left
     });
-
-    var setBlinker = function() {
-      this.setState({
-        caretVisibilityClass: 'blink'
-      });
-    }.bind(this);
 
     if (window.getSelection().type == 'Range') {
       this.setState({
         caretVisibilityClass: 'hidden'
       });
-    } else if (window.getSelection().type == 'Caret') {
-      var timer = setTimeout(setBlinker, 100);
-      timers.push(timer);
+    } else {
+      this.setState({
+        caretVisibilityClass: 'solid'
+      });
     }
   },
 
@@ -53,14 +41,17 @@ var Syntaxarea = React.createClass({
     });
   },
 
+  _showCaret: function(e) {
+    if (window.getSelection().type != 'Range') {
+      this.setState({
+        caretVisibilityClass: 'solid'
+      });
+    }
+  },
+
   _renderOutput: function() {
     var parser = Parser(this.props.rules);
     var tokens = parser.tokenize(this.state.value);
-    var caretIndex;
-
-    if (this.refs.input) {
-      caretIndex = this.refs.input.getDOMNode().selectionStart;
-    }
 
     if (tokens) {
       return spans = tokens.map(function(token, index) {
@@ -79,13 +70,13 @@ var Syntaxarea = React.createClass({
       value: '',
       caretTop: 0,
       caretLeft: 0,
-      caretVisibilityClass: 'blink'
+      caretVisibilityClass: 'solid'
     };
   },
 
   componentDidMount: function() {
-    document.addEventListener('mousedown', this._hideCaret);
     document.addEventListener('select', this._hideCaret);
+    document.addEventListener('keyup', this._updateCaretPosition);
     document.addEventListener('mouseup', this._updateCaretPosition);
   },
 
@@ -93,7 +84,7 @@ var Syntaxarea = React.createClass({
     var output = this._renderOutput();
 
     var caretStyle = {
-      top: this.state.caretTop + 'px',
+      top: (this.state.caretTop - 3) + 'px',
       left: this.state.caretLeft + 'px'
     }
 
@@ -107,11 +98,6 @@ var Syntaxarea = React.createClass({
                   ref='input'
                   autoFocus={true}
                   value={this.state.value}
-                  onKeyPress={this._updateCaretPosition}
-                  onKeyUp={this._updateCaretPosition}
-                  onKeyDown={this._updateCaretPosition}
-                  onFocus={this._setCaretBlinker}
-                  onClick={this._setCaretBlinker}
                   onChange={this._onChange}>
         </TextArea>
 
